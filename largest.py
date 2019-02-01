7489e40d8b9127ea38c151a0e62f730f5332a9e3
Revathi <47189346+Revathi-Chokkalingam@users.noreply.github.com>
2019-02-01 09:32:57 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,9 +1,2 @@
-num1=int(input())
-num2=int(input())
-num3=int(input())
-if (num1>=num2):
-	print('num1')
-elif (num2>=num3):
-	print ('num2')
-else:
-	print ('num3')
+num1, num2, num3 = input().split()
+print(max(num1,num2,num3))
