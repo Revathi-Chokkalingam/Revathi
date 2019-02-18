da437281294c121ae35915b8d411a3e78e1d477b
Revathi <47189346+Revathi-Chokkalingam@users.noreply.github.com>
2019-02-18 17:11:44 +0530

    Update beginner21.c

--- beginner21.c
+++ beginner21.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-int sum=0,A,D;
+int var=0,A,D;
 float N;
 scanf("%f%d%d",&N,&A,&D);
-sum=(N/2)*(2*A+(N-1)*D);
-printf("%d\n",sum);
+var=(N/2)*(2*A+(N-1)*D);
+printf("%d\n",var);
 return 0;
 }
