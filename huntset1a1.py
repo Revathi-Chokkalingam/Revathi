5acee8141bab157ddd330431e8ab83dcc6cd89d0
Revathi <47189346+Revathi-Chokkalingam@users.noreply.github.com>
2019-02-19 23:37:56 +0530

    Create huntset1a1.py

---
+++ huntset1a1.py
@@ -0,0 +1,17 @@
+a=int(input())
+b=[]
+k=0
+l=list(map(int,input().split()))
+l=sorted(l)
+for i in range(len(l)):
+    c=l.count(l[i])
+    if(c>1):
+        if(l[i] not in b):
+            b.append(l[i])
+        i=i+c-1
+        k=k+1
+if(k==0):
+    print("unique")
+else:
+    for i in b:
+        print(i,end=" ")
