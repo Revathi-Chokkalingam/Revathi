f06953196f6975bc974ce5557e4b5b05c8f1e268
Revathi <47189346+Revathi-Chokkalingam@users.noreply.github.com>
2019-02-20 15:38:10 +0530

    Update playset2a2.py

--- playset2a2.py
+++ playset2a2.py
@@ -1,4 +1,6 @@
-x,y=map(int,input().split())
-c=list(map(int,input().split()))
-d=c[-b:]+c[:x-y]
-print(*d)
+import sys, string, math
+n,k1 = map(int,input().split())
+k = k1%n
+L = list(map(int,input().split()))
+L2 = L[-k1:] + L[:-k1]
+print(*L2)
