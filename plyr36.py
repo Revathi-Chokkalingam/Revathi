39927766e8ec28e69c2cccba5098611ad7d82f9b
Revathi <47189346+Revathi-Chokkalingam@users.noreply.github.com>
2019-06-04 16:42:34 +0530

    Update plyr36.py

--- plyr36.py
+++ plyr36.py
@@ -1,6 +1,6 @@
-p,q=map(str,input().split())
+n1,k=map(str,input().split())
 c=0
-for i in range(0,len(p)):
-  if p[i]==q:
+for i in range(0,len(n1)):
+  if n1[i]==k:
     c+=1
-print(c)  
+print(c)   
