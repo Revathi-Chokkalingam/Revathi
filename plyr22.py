18abd0f634b241da98d338fe286dcd255ed9fa54
Revathi <47189346+Revathi-Chokkalingam@users.noreply.github.com>
2019-06-04 15:42:34 +0530

    Update plyr22.py

--- plyr22.py
+++ plyr22.py
@@ -1,6 +1,6 @@
-a,b=map(int,input().split())
+a1,b1=map(int,input().split())
 d=[]
-for i in range (1,b+1):
-    if a%i==0 and b%i==0:
+for i in range (1,b1+1):
+    if a1%i==0 and b1%i==0:
         d.append(i)
 print(d[-1])
