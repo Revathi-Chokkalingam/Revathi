21349b4f8cde42b41d0adc48a7fc1c733e749caa
Revathi <47189346+Revathi-Chokkalingam@users.noreply.github.com>
2019-06-05 20:45:42 +0530

    Update pro15.py

--- pro15.py
+++ pro15.py
@@ -1,10 +1,9 @@
-#reva
 n=int(input())
 l=list(map(int,input().split()))
-l1=[]
+m1=[]
 for i in l:
 	s=bin(i)[2::]
-	l1.append(([s.count("1"),i]))
-l1.sort(reverse=True)
-for i in range(0,len(l1)):
-	print(l1[i][1])
+	m1.append(([s.count("1"),i]))
+m1.sort(reverse=True)
+for i in range(0,len(m1)):
+	print(m1[i][1])
